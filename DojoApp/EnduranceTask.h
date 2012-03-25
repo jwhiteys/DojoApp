69ff15c4073e73565234176076ecaa6e8ac3b4cf
Justin Skweres <justin.skweres@yahoo.com>
2012-03-25 01:45:12 -0400

    the first commit.  Switched the name back to Dojo from DojoApp

---
+++ DojoApp/EnduranceTask.h
@@ -0,0 +1,19 @@
+//
+//  EnduranceTask.h
+//  Dojo
+//
+//  Created by Justin Skweres on 3/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+#import "BasicTask.h"
+
+
+@interface EnduranceTask : BasicTask
+
+@property (nonatomic, retain) NSNumber * isEnduranceTask;
+@property (nonatomic, retain) NSNumber * totalTime;
+
+@end
---
+++ DojoApp/EnduranceTask.m
@@ -0,0 +1,17 @@
+//
+//  EnduranceTask.m
+//  Dojo
+//
+//  Created by Justin Skweres on 3/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "EnduranceTask.h"
+
+
+@implementation EnduranceTask
+
+@dynamic isEnduranceTask;
+@dynamic totalTime;
+
+@end
