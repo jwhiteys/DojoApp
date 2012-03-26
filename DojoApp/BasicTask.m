402286414ebd3bbbf55cd8ef56c824be0fdf206f
Justin Skweres <justin.skweres@yahoo.com>
2012-03-25 22:20:00 -0400

    dataManager interface

---
+++ DojoApp/BasicTask.m
@@ -0,0 +1,27 @@
+//
+//  BasicTask.m
+//  Dojo
+//
+//  Created by Justin Skweres on 3/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "BasicTask.h"
+#import "Goal.h"
+
+
+@implementation BasicTask
+
+@dynamic dingCount;
+@dynamic endTime;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic productivity;
+@dynamic quantCount;
+@dynamic quantUnitType;
+@dynamic startTime;
+@dynamic usesQuants;
+@dynamic isCompleted;
+@dynamic goal;
+
+@end
